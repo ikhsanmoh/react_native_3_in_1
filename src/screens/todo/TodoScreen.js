9ae2ac4c311baaa5b3c197303cd66b59b0ae92af
Ikhsan Mohamad <icanmohamad13@gmail.com>
2021-07-04 13:31:37 +0700

    Add note as comment

--- src/screens/todo/TodoScreen.js
+++ src/screens/todo/TodoScreen.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+/**
+ * TODO 1: Init neccessary components.
+ * TODO 2: Set todo app base design.
+ * TODO 3: Style main theme/color.
+ * TODO 4: Set todo app functionality.
+ * TODO 5: Test.
+ * ! Est: 2-3 h.
+ */
+
 const TodoScreen = () => {
   return (
     <View>
